b43c21c1cb296c1e43efbb500920af7da2b2cd5a
Aakash <aakashsharma.aec@gmail.com>
2023-03-03 19:52:05 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
+/// <reference types="@angular/localize" />
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { config } from 'rxjs';
+
 
 import { AppModule } from './app/app.module';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+
+
